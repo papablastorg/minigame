4061f54e78fe1d56ed0d7883388dd704946ac247
underbeerik <mishenkoartem26@gmail.com>
2025-04-16 03:12:07 +0300

    add UI for airdrop, referral, leaderboard pages

--- src/components/AirDrop/index.tsx
+++ src/components/AirDrop/index.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import { Telegram } from '../icons';
+import styles from './AirDrop.module.css';
 
-export const AirDrop: React.FC = () => {
+export const AirDrop = () => {
   return (
-    <div>
-      <h1>AirDrop</h1>
-      {/* Add AirDrop content here */}
-    </div>
+    <div className={styles.airDropContainer}>
+        <div className={styles.header}>AIRDROP</div>
+        <p className={styles.comingSoon}>Cooming soon...</p>
+        <img width={300} height={300} src="images/airdrop.png" alt="AirDrop" className={styles.airDropImage} />
+        <div className={styles.airDropButtonContainer}>
+          <button onClick={() => window.open('https://t.me/blum/app?startapp=memepadjetton_PAPA_WMm5z-ref_i6eMS3S5Fj', '_blank')} className={styles.joinChanelButton}>
+            <Telegram />
+            Join Chanel
+          </button>
+        </div>
+      </div>
   );
 };
